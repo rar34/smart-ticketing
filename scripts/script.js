6aee2f6224772349a915eae4ef9c60087183260b
rar34 <ra.cse34@gmail.com>
2024-02-18 10:48:16 +0600

    make ticket section dynamic

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,58 @@
+const couponButton = document.getElementById('btn-coupon');
+couponButton.setAttribute('disabled', true);
+const nextButton = document.getElementById('btn-next');
+nextButton.setAttribute('disabled', true);
 const seats = document.getElementsByClassName('seat');
-for(const seat of seats){
-    seat.addEventListener('click',function(){
-        console.log('clicked')
+const perSeatPrice = 550;
+// total seat count
+const totalNumberSeat = document.getElementById('available-seat').innerText;
+let availableSeat = parseInt(totalNumberSeat);
+// console.log(availableSeat);
+
+let count = 0;
+let totalPrice = 0;
+for (const seat of seats) {
+    seat.addEventListener('click', function (event) {
+        count++;
+
         seat.style.backgroundColor = 'green';
-        seat.setAttribute('disabled', true)
+        seat.setAttribute('disabled', true);
+        availableSeat = availableSeat - 1;
+        console.log(availableSeat)
+        const seatName = event.target.innerText;
+
+        totalPrice = perSeatPrice * count;
+        if (count > 4) {
+            seat.style.backgroundColor = '#F7F8F8';
+            return alert('You can not buy more than 4 ticket');
+        }
+        else {
+            const tBody = document.getElementById('tbody');
+            const tr = document.createElement('tr');
+            const th1 = document.createElement('th');
+            th1.innerText = seatName;
+            const th2 = document.createElement('th');
+            th2.innerText = 'Economy';
+            const th3 = document.createElement('th');
+            th3.innerText = perSeatPrice;
+            tr.appendChild(th1);
+            tr.appendChild(th2);
+            tr.appendChild(th3);
+            tBody.appendChild(tr);
+            tr.classList.add('flex', 'justify-between')
+        }
+
+
+
+        setValueById('total-price', totalPrice);
+        setValueById('available-seat', availableSeat)
     })
+}
+
+
+
+// setValueById
+
+function setValueById(id, value) {
+    document.getElementById(id).innerText = value;
 }
